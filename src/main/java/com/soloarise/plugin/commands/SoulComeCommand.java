2c2058e281394bbf6c5b31f5ac57165d57ad6821
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:18:27 +0530

    Add SoulComeCommand for soul recalling functionality
    Implement SoulComeCommand to allow players to recall souls.

--- src/main/java/com/soloarise/plugin/commands/SoulComeCommand.java
+++ src/main/java/com/soloarise/plugin/commands/SoulComeCommand.java
@@ -1,3 +1,27 @@
 package com.soloarise.plugin.commands;
 
-public class SoulComeCommand {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SoulComeCommand implements CommandExecutor {
+    
+    private final SoloArisePlugin plugin;
+    
+    public SoulComeCommand(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("§cOnly players can use this command!");
+            return true;
+        }
+        
+        plugin.getSoulManager().recallSouls(player);
+        return true;
+    }
+}
