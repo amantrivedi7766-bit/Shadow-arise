20b2061fac8a1f2e10b82d0353e516f5161b1100
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:17:39 +0530

    Add SoulAttackCommand class for player attack command
    Implement SoulAttackCommand to handle player attacks.

--- src/main/java/com/soloarise/plugin/commands/SoulAttackCommand.java
+++ src/main/java/com/soloarise/plugin/commands/SoulAttackCommand.java
@@ -1,3 +1,33 @@
 package com.soloarise.plugin.commands;
 
-public class SoulAttackCommand {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SoulAttackCommand implements CommandExecutor {
+    
+    private final SoloArisePlugin plugin;
+    
+    public SoulAttackCommand(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("§cOnly players can use this command!");
+            return true;
+        }
+        
+        if (args.length < 1) {
+            player.sendMessage("§cUsage: /soulattack <playername>");
+            return true;
+        }
+        
+        String targetName = args[0];
+        plugin.getSoulManager().attackPlayer(player, targetName);
+        return true;
+    }
+}
