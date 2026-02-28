9f73fdc14877a41cd82f6f34b60bf3f83146a25d
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:55:39 +0530

    Refactor SoloArisePlugin to extend JavaPlugin
    Refactored SoloArisePlugin to extend JavaPlugin and added initialization for various managers and command registration.

--- src/main/java/com/soloarise/plugin/SoloArisePlugin.java
+++ src/main/java/com/soloarise/plugin/SoloArisePlugin.java
@@ -1,35 +1,91 @@
-package com.soloarise.plugin.commands;
+package com.soloarise.plugin;
 
-import com.soloarise.plugin.SoloArisePlugin;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.soloarise.plugin.listeners.*;
+import com.soloarise.plugin.managers.*;
+import com.soloarise.plugin.commands.*;
 
-public class AriseWorkCommand implements CommandExecutor {
+public class SoloArisePlugin extends JavaPlugin {
     
-    private final SoloArisePlugin plugin;
+    private static SoloArisePlugin instance;
+    private PlayerManager playerManager;
+    private SoulManager soulManager;
+    private TaskManager taskManager;
+    private ConfigManager configManager;
+    private DatabaseManager databaseManager;
+    private ScoreboardManager scoreboardManager;
+    private ParticleManager particleManager;
     
-    public AriseWorkCommand(SoloArisePlugin plugin) {
-        this.plugin = plugin;
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Initialize configurations
+        saveDefaultConfig();
+        configManager = new ConfigManager(this);
+        
+        // Initialize managers
+        databaseManager = new DatabaseManager(this);
+        playerManager = new PlayerManager(this);
+        soulManager = new SoulManager(this);
+        taskManager = new TaskManager(this);
+        scoreboardManager = new ScoreboardManager(this);
+        particleManager = new ParticleManager(this);
+        
+        // Register commands
+        registerCommands();
+        
+        // Register listeners
+        registerListeners();
+        
+        // Start scheduler for boss bar updates
+        startBossBarScheduler();
+        
+        getLogger().info("SoloArisePlugin has been enabled!");
     }
     
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("§cOnly players can use this command!");
-            return true;
+    public void onDisable() {
+        if (databaseManager != null) {
+            databaseManager.saveAll();
         }
-        
-        if (args.length < 2) {
-            player.sendMessage("§cUsage: /arisework <soulname> <workorder>");
-            return true;
-        }
-        
-        String soulName = args[0];
-        String workOrder = String.join(" ", java.util.Arrays.copyOfRange(args, 1, args.length));
-        
-        plugin.getSoulManager().orderWork(player, soulName, workOrder);
-        return true;
+        getLogger().info("SoloArisePlugin has been disabled!");
     }
+    
+    private void registerCommands() {
+        getCommand("arise").setExecutor(new AriseCommand(this));
+        getCommand("arisework").setExecutor(new AriseWorkCommand(this));
+        getCommand("soulrelease").setExecutor(new SoulReleaseCommand(this));
+        getCommand("soulattack").setExecutor(new SoulAttackCommand(this));
+        getCommand("soulcome").setExecutor(new SoulComeCommand(this));
+        getCommand("soultask").setExecutor(new SoulTaskCommand(this));
+    }
+    
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+        getServer().getPluginManager().registerEvents(new MobDeathListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerChatListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
+    }
+    
+    private void startBossBarScheduler() {
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            if (taskManager != null) {
+                taskManager.updateAllBossBars();
+            }
+        }, 0L, 20L);
+    }
+    
+    public static SoloArisePlugin getInstance() {
+        return instance;
+    }
+    
+    public PlayerManager getPlayerManager() { return playerManager; }
+    public SoulManager getSoulManager() { return soulManager; }
+    public TaskManager getTaskManager() { return taskManager; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public DatabaseManager getDatabaseManager() { return databaseManager; }
+    public ScoreboardManager getScoreboardManager() { return scoreboardManager; }
+    public ParticleManager getParticleManager() { return particleManager; }
 }
