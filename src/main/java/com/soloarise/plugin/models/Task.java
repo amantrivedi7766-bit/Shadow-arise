a03b8d7378947752931835bc2b45fae1496c5034
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:28:56 +0530

    Implement Task class with completion condition
    Add fields and methods for task management.

--- src/main/java/com/soloarise/plugin/models/Task.java
+++ src/main/java/com/soloarise/plugin/models/Task.java
@@ -1,3 +1,24 @@
 package com.soloarise.plugin.models;
 
-public class Task {}
+import org.bukkit.entity.Player;
+import java.util.function.Predicate;
+
+public class Task {
+    
+    private final int id;
+    private final String name;
+    private final Predicate<Player> completionCondition;
+    
+    public Task(int id, String name, Predicate<Player> completionCondition) {
+        this.id = id;
+        this.name = name;
+        this.completionCondition = completionCondition;
+    }
+    
+    public int getId() { return id; }
+    public String getName() { return name; }
+    
+    public boolean isComplete(Player player) {
+        return completionCondition.test(player);
+    }
+}
