1713cd51bc9f8a2d4b68e6bf3d874cd797688372
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:15:03 +0530

    Implement AriseWorkCommand for player commands
    Added AriseWorkCommand class to handle work commands from players.

--- src/main/java/com/soloarise/plugin/SoloArisePlugin.java
+++ src/main/java/com/soloarise/plugin/SoloArisePlugin.java
@@ -1,3 +1,35 @@
-package com.soloarise.plugin;
+package com.soloarise.plugin.commands;
 
-public class SoloArisePlugin {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AriseWorkCommand implements CommandExecutor {
+    
+    private final SoloArisePlugin plugin;
+    
+    public AriseWorkCommand(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("§cOnly players can use this command!");
+            return true;
+        }
+        
+        if (args.length < 2) {
+            player.sendMessage("§cUsage: /arisework <soulname> <workorder>");
+            return true;
+        }
+        
+        String soulName = args[0];
+        String workOrder = String.join(" ", java.util.Arrays.copyOfRange(args, 1, args.length));
+        
+        plugin.getSoulManager().orderWork(player, soulName, workOrder);
+        return true;
+    }
+}
