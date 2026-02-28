9f551466e619a51a101c3e23b5369738a26247b5
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:21:25 +0530

    Add MobDeathListener to handle entity death events
    Implement MobDeathListener to handle mob death events and manage player actions.

--- src/main/java/com/soloarise/plugin/listeners/MobDeathListener.java
+++ src/main/java/com/soloarise/plugin/listeners/MobDeathListener.java
@@ -1,3 +1,27 @@
 package com.soloarise.plugin.listeners;
 
-public class MobDeathListener {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class MobDeathListener implements Listener {
+    
+    private final SoloArisePlugin plugin;
+    
+    public MobDeathListener(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onMobDeath(EntityDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        Player killer = entity.getKiller();
+        
+        if (killer != null && plugin.getPlayerManager().getPlayer(killer).hasArisePower()) {
+            plugin.getSoulManager().addKillToCaptureQueue(killer, entity);
+        }
+    }
+}
