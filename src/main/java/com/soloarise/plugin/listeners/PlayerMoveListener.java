f694813b9693b5730c3fdaae5579d376ad40d0c2
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-02-28 19:22:57 +0530

    Add PlayerMoveListener to handle player movement events
    Implement PlayerMoveListener to track player movement.

--- src/main/java/com/soloarise/plugin/listeners/PlayerMoveListener.java
+++ src/main/java/com/soloarise/plugin/listeners/PlayerMoveListener.java
@@ -1,3 +1,23 @@
 package com.soloarise.plugin.listeners;
 
-public class PlayerMoveListener {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PlayerMoveListener implements Listener {
+    
+    private final SoloArisePlugin plugin;
+    
+    public PlayerMoveListener(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (event.getFrom().distanceSquared(event.getTo()) > 0) {
+            double distance = event.getFrom().distance(event.getTo());
+            plugin.getPlayerManager().getPlayer(event.getPlayer()).addDistanceTraveled((int) distance);
+        }
+    }
+}
