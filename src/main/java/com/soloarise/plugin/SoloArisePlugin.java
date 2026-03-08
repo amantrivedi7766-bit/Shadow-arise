08f1303048ca998efb1e6b51e9781e70a7c4c5d6
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-08 12:50:53 +0530

    Update SoloArisePlugin.java

--- src/main/java/com/soloarise/plugin/SoloArisePlugin.java
+++ src/main/java/com/soloarise/plugin/SoloArisePlugin.java
@@ -1,9 +1,7 @@
 package com.soloarise.plugin;
 
 import com.soloarise.plugin.commands.*;
-import com.soloarise.plugin.commands.admin.AdminPanelCommand;
-import com.soloarise.plugin.commands.tabcompleters.AdminTabCompleter;
-import com.soloarise.plugin.commands.tabcompleters.SummonTabCompleter;
+import com.soloarise.plugin.commands.admin.AdminChestCommand;
 import com.soloarise.plugin.listeners.*;
 import com.soloarise.plugin.managers.*;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,13 +12,12 @@ public class SoloArisePlugin extends JavaPlugin {
     
     // Managers
     private ConfigManager configManager;
-    private DatabaseManager databaseManager;
-    private PlayerManager playerManager;
     private SoulManager soulManager;
-    private TaskManager taskManager;
     private ScoreboardManager scoreboardManager;
     private ParticleManager particleManager;
-    private SummonSessionManager summonSessionManager;
+    private AbilityManager abilityManager;
+    private HeadManager headManager;
+    private AdminMenuManager adminMenuManager;
     
     @Override
     public void onEnable() {
@@ -31,13 +28,12 @@ public class SoloArisePlugin extends JavaPlugin {
         configManager = new ConfigManager(this);
         
         // Initialize managers
-        databaseManager = new DatabaseManager(this);
-        playerManager = new PlayerManager(this);
         soulManager = new SoulManager(this);
-        taskManager = new TaskManager(this);
         scoreboardManager = new ScoreboardManager(this);
         particleManager = new ParticleManager(this);
-        summonSessionManager = new SummonSessionManager(this);
+        abilityManager = new AbilityManager(this);
+        headManager = new HeadManager(this);
+        adminMenuManager = new AdminMenuManager(this);
         
         // Register commands
         registerCommands();
@@ -45,44 +41,29 @@ public class SoloArisePlugin extends JavaPlugin {
         // Register listeners
         registerListeners();
         
-        // Start scheduler for boss bar updates
-        startBossBarScheduler();
+        // Start update schedulers
+        startSchedulers();
         
-        getLogger().info("SoloArisePlugin has been enabled!");
-        getLogger().info("Version: 1.0.0");
-        getLogger().info("Made for Minecraft 1.21.4");
+        getLogger().info("§a✓ SoloArise Plugin Enabled!");
+        getLogger().info("§7Version: 1.0.0");
     }
     
     @Override
     public void onDisable() {
-        // Save all player data
-        if (databaseManager != null) {
-            databaseManager.saveAll();
-        }
-        
         // Recall all summoned souls
         if (soulManager != null) {
             soulManager.recallAllSouls();
         }
         
-        getLogger().info("SoloArisePlugin has been disabled!");
+        getLogger().info("§c✗ SoloArise Plugin Disabled!");
     }
     
     private void registerCommands() {
-        // Main commands
         getCommand("arise").setExecutor(new AriseCommand(this));
-        getCommand("arisework").setExecutor(new AriseWorkCommand(this));
-        getCommand("soulrelease").setExecutor(new SoulReleaseCommand(this));
-        getCommand("soulattack").setExecutor(new SoulAttackCommand(this));
         getCommand("soulcome").setExecutor(new SoulComeCommand(this));
-        getCommand("soultask").setExecutor(new SoulTaskCommand(this));
-        getCommand("summon").setExecutor(new SummonCommand(this));
-        getCommand("healsoul").setExecutor(new HealSoulCommand(this));
-        getCommand("solarise").setExecutor(new AdminPanelCommand(this));
-        
-        // Register tab completers
-        getCommand("summon").setTabCompleter(new SummonTabCompleter(this));
-        getCommand("solarise").setTabCompleter(new AdminTabCompleter(this));
+        getCommand("soulrelease").setExecutor(new SoulReleaseCommand(this));
+        getCommand("soulability").setExecutor(new SoulAbilityCommand(this));
+        getCommand("solarise").setExecutor(new AdminChestCommand(this));
     }
     
     private void registerListeners() {
@@ -90,8 +71,7 @@ public class SoloArisePlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerChatListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerCrouchListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
         
         // Entity listeners
@@ -99,21 +79,12 @@ public class SoloArisePlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EntityDamageListener(this), this);
         getServer().getPluginManager().registerEvents(new EntityTargetListener(this), this);
         
-        // World listeners
-        getServer().getPluginManager().registerEvents(new WorldListener(this), this);
-        
-        // Inventory listeners
-        getServer().getPluginManager().registerEvents(new InventoryListener(this), this);
+        // Menu listeners
+        getServer().getPluginManager().registerEvents(new MenuClickListener(this), this);
+        getServer().getPluginManager().registerEvents(new AdminMenuListener(this), this);
     }
     
-    private void startBossBarScheduler() {
-        // Update boss bars every second
-        getServer().getScheduler().runTaskTimer(this, () -> {
-            if (taskManager != null) {
-                taskManager.updateAllBossBars();
-            }
-        }, 20L, 20L);
-        
+    private void startSchedulers() {
         // Update scoreboards every 2 seconds
         getServer().getScheduler().runTaskTimer(this, () -> {
             if (scoreboardManager != null) {
@@ -121,48 +92,21 @@ public class SoloArisePlugin extends JavaPlugin {
             }
         }, 40L, 40L);
         
-        // Check for expired tasks every minute
+        // Update soul AI every second
         getServer().getScheduler().runTaskTimer(this, () -> {
-            if (playerManager != null) {
-                playerManager.checkExpiredTasks();
+            if (soulManager != null) {
+                soulManager.updateAllSouls();
             }
-        }, 1200L, 1200L);
+        }, 20L, 20L);
     }
     
-    // Getters for all managers
-    public static SoloArisePlugin getInstance() {
-        return instance;
-    }
-    
-    public ConfigManager getConfigManager() {
-        return configManager;
-    }
-    
-    public DatabaseManager getDatabaseManager() {
-        return databaseManager;
-    }
-    
-    public PlayerManager getPlayerManager() {
-        return playerManager;
-    }
-    
-    public SoulManager getSoulManager() {
-        return soulManager;
-    }
-    
-    public TaskManager getTaskManager() {
-        return taskManager;
-    }
-    
-    public ScoreboardManager getScoreboardManager() {
-        return scoreboardManager;
-    }
-    
-    public ParticleManager getParticleManager() {
-        return particleManager;
-    }
-    
-    public SummonSessionManager getSummonSessionManager() {
-        return summonSessionManager;
-    }
+    // Getters
+    public static SoloArisePlugin getInstance() { return instance; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public SoulManager getSoulManager() { return soulManager; }
+    public ScoreboardManager getScoreboardManager() { return scoreboardManager; }
+    public ParticleManager getParticleManager() { return particleManager; }
+    public AbilityManager getAbilityManager() { return abilityManager; }
+    public HeadManager getHeadManager() { return headManager; }
+    public AdminMenuManager getAdminMenuManager() { return adminMenuManager; }
 }
