3eec54e7b943e7a1ba64ec1214f19cafbb0b25fe
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-08 11:37:48 +0530

    Update EntityDamageListener.java

--- src/main/java/com/soloarise/plugin/listeners/EntityDamageListener.java
+++ src/main/java/com/soloarise/plugin/listeners/EntityDamageListener.java
@@ -1,3 +1,27 @@
 package com.soloarise.plugin.listeners;
 
-public class EntityDamageListener {}
+import com.soloarise.plugin.SoloArisePlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class EntityDamageListener implements Listener {
+    
+    private final SoloArisePlugin plugin;
+    
+    // Add constructor with plugin parameter
+    public EntityDamageListener(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        // Handle damage logic
+    }
+    
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        // Handle damage by entity logic
+    }
+}
