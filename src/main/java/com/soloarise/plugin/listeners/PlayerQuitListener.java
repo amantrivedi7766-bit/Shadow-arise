0ad1eaf71e8fce1fee09636eb6f23d93757c9a2b
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-08 12:02:13 +0530

    Create PlayerQuitListener.java

---
+++ src/main/java/com/soloarise/plugin/listeners/PlayerQuitListener.java
@@ -0,0 +1,41 @@
+package com.soloarise.plugin.listeners;
+
+import com.soloarise.plugin.SoloArisePlugin;
+import com.soloarise.plugin.models.CapturedSoul;
+import com.soloarise.plugin.models.PlayerSoulData;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+    
+    private final SoloArisePlugin plugin;
+    
+    public PlayerQuitListener(SoloArisePlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        
+        // Recall all summoned souls
+        PlayerSoulData soulData = plugin.getSoulManager().getPlayerSouls().get(player.getUniqueId());
+        if (soulData != null) {
+            for (CapturedSoul soul : soulData.getSummonedSouls()) {
+                if (soul.getSummonedEntity() != null) {
+                    soul.getSummonedEntity().remove();
+                }
+                soul.setSummoned(false);
+                soul.setSummonedEntity(null);
+            }
+        }
+        
+        // Clean up sessions
+        plugin.getSummonSessionManager().endSession(player);
+        
+        // Remove from active tasks
+        plugin.getPlayerManager().removeActiveTask(player.getUniqueId());
+    }
+}
